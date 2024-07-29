8adec557a7bccb5e32648a271403a2c1115d9b6a
chun <815818600@qq.com>
2024-07-29 11:15:29 +0800

    coremark

---
+++ riscv_se_ruby.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./build/RISCV/gem5.debug \
+    -d /home/cwh/work/benchmark/data/coremark-pro/ruby/radix \
+    ./configs/deprecated/example/se.py \
+    -n 16 \
+    --cpu-type TimingSimpleCPU  \
+    --l1d_size=64kB \
+    --l1i_size=64kB \
+    --num-l2caches=16 \
+    --l2_size=128kB \
+    --num-dirs=16 \
+    --ruby \
+    --network=garnet \
+    --topology=Crossbar \
+    -c /home/cwh/work/benchmark/coremark-pro/builds/linux64/gcc64-riscv/bin/radix2-big-64k.exe
